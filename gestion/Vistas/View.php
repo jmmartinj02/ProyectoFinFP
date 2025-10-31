69feec784a9f39ae4f7ab24c4e8192ad750dc567
jmmartinj02 <146434706+jmmartinj02@users.noreply.github.com>
2025-10-31 17:31:44 +0100

    Delete gestion directory

--- gestion/Vistas/View.php
+++
@@ -1,7 +0,0 @@
-<?php
-class View {
-    public static function show($viewName, $data = []) {
-        extract($data);
-        require __DIR__ . "/$viewName.php";
-    }
-}
--- gestion/includes/footer.php
+++
@@ -1,5 +0,0 @@
-</div>
-<footer class="bg-dark text-center text-light py-2 fixed-bottom">
-  <small>© <?= date('Y') ?> Gestor Remoto BD - José M. Martín</small>
-</footer>
-</html>
